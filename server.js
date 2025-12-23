132cd23b91c445372a15da9976d8be7ff05b93ff
riverclack <it@riverclack.net>
2025-12-23 13:44:01 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,15 +2,23 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-// Разрешаем получать данные из папки public (если вы захотите добавить картинки/css)
+// Позволяем серверу читать данные, которые присылает Битрикс (POST form data)
+app.use(express.urlencoded({ extended: true })); 
 app.use(express.static(__dirname));
 
-// Самое главное: на любой запрос (GET или POST) отдаем ваш HTML файл
-app.all("/", (req, res) => {
+// ГЛАВНЫЙ ФОКУС:
+// Если Битрикс шлет POST запрос (при открытии вкладки)
+app.post("/", (req, res) => {
+    // Мы берем все данные и перенаправляем их сами на себя, но уже как GET запрос
+    const query = new URLSearchParams(req.body).toString();
+    res.redirect("/?" + query);
+});
+
+// Если это обычный GET запрос (или редирект от поста выше)
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
-// Запускаем сервер
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
